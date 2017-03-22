71e7fc26f53e7e9dcd43e37613545af2ee373f09
Malachi Burke <malachi.burke@gmail.com>
2017-03-22 16:36:43 -0700

    Boilerplate checkin for buffered soft serial mbed test console

---
+++ test/mbed/console/main.cpp
@@ -0,0 +1,57 @@
+#include <mbed.h>
+#include <mbed_events.h>
+
+#include <BufferedSoftSerial.h>
+
+#define MAL_LED LED1
+
+static void blinky(void) {
+    static DigitalOut led(MAL_LED);
+    led = !led;
+    //printf("LED = %d \r\n",led.read());
+}
+
+
+BufferedSoftSerial serial(PA_10, PB_3);
+Serial usb(USBTX, USBRX);
+
+/*
+static void echo()
+{
+    for(;;)
+    {
+        if(serial.readable())
+        {
+            int c = serial.getc();
+            usb.putc(c);
+        }
+        else if(usb.readable())
+        {
+            int c = usb.getc();
+            serial.putc(c);
+        }
+        else
+        {
+            // 10 ms
+            Thread::wait(10);
+        }
+    }
+}
+*/
+
+int main()
+{
+    Thread echoThread;
+
+    EventQueue queue;
+
+    printf("Compiled at: " __TIME__ "\r\n");
+
+    serial.baud(9600);
+
+    //echoThread.start(echo);
+
+    queue.call_every(1000, blinky);
+
+    queue.dispatch();
+}
