1026599d1853e0b0334b4df53476bf47fc49259b
Malachi Burke <malachi.burke@gmail.com>
2017-03-22 02:01:53 -0700

    More fiddling with usb posix-console
    Looks like this too is gonna be a lot of work....

--- test/posix-console/main.cpp
+++ test/posix-console/main.cpp
@@ -4,14 +4,29 @@
 // copying basic concepts for newb thread: http://forum.arduino.cc/index.php?topic=185031.0
 // this didn't help: stty -f /dev/tty.usbserial-AH00R97V sane raw pass8 -echo -hupcl clocal 115200
 
+// http://stackoverflow.com/questions/11677639/two-way-c-communication-over-serial-connection
+// http://stackoverflow.com/questions/109449/getting-a-file-from-a-stdfstream
+
+
+#include <unistd.h>
+
 using namespace std;
 
 void raw_console()
 {
     char ch;
+
+    // hacky way to get FD for the ifstream.  Not proven to work
+    // but hopefully we can use it to set usb/baud rate parameters
+    auto fd = dup(0);
+    close(fd);
+
     ifstream f;
+
     //f.open("/dev/ttyACM0");
     f.open("/dev/tty.usbserial-AH00R97V");
+
+    cout << "USB opened" << endl;
 
     while (f.get(ch))
     {
