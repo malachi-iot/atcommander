6f2b82fa3e87f89badaabc864a7ecb01e16b8b16
Malachi Burke <malachi.burke@gmail.com>
2017-03-22 00:42:03 -0700

    Got a basic USB enumerator working
    Only to discover it actually *is* easy to use USB serial with iostreams...bleh

--- test/linux.console/main.cpp
+++ test/linux.console/main.cpp
@@ -1,16 +1,49 @@
 #include <iostream>
-#include <libusb.h>
-
+//#include <libusb.h>
+#include <libusb-1.0/libusb.h>
+//#include <libusbpp.hpp>
+#include <cassert>
+#include <cstdio>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+    //auto devices = LibUSB::LibUSB::FindAllDevices();
     libusb_context* ctx;
+    libusb_device** device_list;
 
     libusb_init(&ctx);
+    auto count = libusb_get_device_list(ctx, &device_list);
+
+    assert(count > 0);
+
+    for (size_t idx = 0; idx < count; ++idx) {
+        libusb_device *device = device_list[idx];
+        libusb_device_descriptor desc = {0};
+
+        auto rc = libusb_get_device_descriptor(device, &desc);
+        assert(rc == 0);
+
+        printf("Vendor:Device = %04x:%04x\n", desc.idVendor, desc.idProduct);
+    }
+
+/*
+    if(device_list != nullptr)
+    {
+        libusb_device* device = device_list[0];
+        if(device != nullptr)
+        {
+            libusb_device_descriptor device_descriptor;
+            libusb_get_device_descriptor(device, &device_descriptor);
+            auto idProduct = device_descriptor.idProduct;
+            cout << idProduct;
+        }
+    }
+    */
     cout << "Hello World!" << endl;
     return 0;
 
+    libusb_free_device_list(device_list, 1);
     libusb_exit(ctx);
 }
